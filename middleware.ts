9958ab156dd630b10fee69f91a2335ebfd8d1f09
aisyahrr <aisyahr142@gmail.com>
2026-01-16 22:06:33 +0700

    update route

--- middleware.ts
+++ middleware.ts
@@ -20,6 +20,7 @@ export function middleware(request: NextRequest) {
     }
 
     return NextResponse.next();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return NextResponse.next();
   }
